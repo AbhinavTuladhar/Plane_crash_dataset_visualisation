2954f9c5e0e790967a6aebc3e567297f719ee149
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-03-13 18:46:32 +0545

    Completed majority of crashes page

--- pages/Crashes.py
+++ pages/Crashes.py
@@ -4,6 +4,8 @@ import plotly.graph_objects as go
 import pandas as pd
 from utils import aggregate_columns, find_crash_counts
 import random
+import json
+import numpy as np
 
 st.set_page_config(layout="wide")
 
@@ -18,6 +20,7 @@ with open('Colours_list_real.txt', 'r') as file:
 # For the continous colour scales (for heatmaps)
 continuous_colours = px.colors.named_colorscales()
 continuous_colours.sort()
+
 # Find the index of the default scale - "Jet"
 heatmap_colour_default = continuous_colours.index('jet')
 
@@ -25,16 +28,11 @@ with st.sidebar:
     colour_choice = st.selectbox(
         label='Use CSS colours or hex colours?',
         options=['CSS', 'Hex'],
-        index=1
+        index=0
     )
     if colour_choice == 'Hex':
-        background_colour = st.color_picker(label='Background colour of the plots')
         plot_colour = st.color_picker(label='Colour to be used in the plots')
     else:
-        background_colour = st.selectbox(
-            label='Select the background colour',
-            options=CSS_colours
-        )
         plot_colour = st.selectbox(
             label='Select the plot colour',
             options=CSS_colours
@@ -49,7 +47,6 @@ with st.sidebar:
 template = go.layout.Template()
 template.layout.font.color = 'white'
 template.layout.paper_bgcolor = '#1E1E1E'
-template.layout.plot_bgcolor = background_colour
 px.defaults.template = template
 
 st.markdown('# Number of crashes')
@@ -69,7 +66,6 @@ def crashes_per_year():
     )
     st.plotly_chart(fig, use_container_width=True)
 
-
 def crashes_per_day_number():
     st.markdown('## Crashes in each day number')
     df_day_number = find_crash_counts(df=df, grouping_cols=df['Date'].dt.day, date_name='Day')
@@ -84,7 +80,7 @@ def crashes_per_day_number():
         color_discrete_sequence=[plot_colour]
     ).update_layout(bargap=0.05).update_yaxes(title_text='Crashes')
     st.plotly_chart(fig, use_container_width=True)
- 
+
 def crashes_per_month():
     st.markdown('## Crashes in each month')
     df_month = find_crash_counts(df=df, grouping_cols='Month')
@@ -100,7 +96,7 @@ def crashes_per_month():
         height=500
     ).update_layout(bargap=0.2).update_yaxes(title_text='Crashes')
     st.plotly_chart(fig, use_container_width=True)
-    
+   
 def crashes_per_day_of_week():
     st.markdown('## Crashes in each day')
     df_day = find_crash_counts(df=df, grouping_cols='Day_of_week')
@@ -116,7 +112,7 @@ def crashes_per_day_of_week():
         height=500
     ).update_layout(bargap=0.2).update_yaxes(title_text='Crashes')
     st.plotly_chart(fig, use_container_width=True)
-    
+   
 def crashes_per_decade():
     st.markdown('## Crashes per decade')
     df_decade = find_crash_counts(df=df, grouping_cols='Decade')
@@ -138,8 +134,8 @@ def crashes_per_decade():
         height=500
     ).update_layout(bargap=0.2).update_yaxes(title_text='Crashes')
     st.plotly_chart(fig, use_container_width=True)
-    
-def crashes_per_year_and_month(base_decade, separation):
+
+def crashes_per_year_and_month(base_decade, separation, width=None):
     """
     Shows a heatmap of crashes per year and month of two decades.
     """
@@ -161,7 +157,7 @@ def crashes_per_year_and_month(base_decade, separation):
         aspect='equal', 
         color_continuous_scale=heatmap_colour,
         title=f'Crashes from {decade_values[0]} till {decade_values[-1]+9}',
-        width=100, height=500
+        width=width, height=500
     )
     st.plotly_chart(fig, use_container_width=True)
 
@@ -175,11 +171,10 @@ def crashes_per_month_weekday():
         color_continuous_scale=heatmap_colour,
         title=f'Crashes by month and day of week',
         text_auto=True,
-        width=400, height=500
+        height=500
     )
     st.plotly_chart(fig, use_container_width=True)
-    
-    
+       
 def crashes_per_month_day():
     st.markdown('## Per month and day number')
     df_month_day = find_crash_counts(df=df, grouping_cols=['Month', df['Date'].dt.day], date_name='Day')
@@ -190,15 +185,86 @@ def crashes_per_month_day():
         color_continuous_scale=heatmap_colour,
         title=f'Crashes by month and day number',
         text_auto=True,
-        width=400, height=500
+        height=500
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
+def crashes_by_country_map(us_exclude_flag):
+    crash_counts = find_crash_counts(df=df, grouping_cols='Country')
+    if us_exclude_flag:
+        crash_counts = crash_counts.query('Country != "United States of America"')
+
+    fig = px.choropleth(
+        crash_counts,
+        locations='Country',
+        locationmode='country names',
+        color='Crashes',
+        hover_name='Country',
+        color_continuous_scale=heatmap_colour
+    ).update_layout(geo=dict(showocean=True, oceancolor='LightBlue'),
+        title=dict(x=0.5),
+        margin=dict(l=0, r=0, t=50, b=0)
     )
     st.plotly_chart(fig, use_container_width=True)
     
+
+    
+def crashes_by_country_treemap(us_exclude_flag):
+    crash_counts = find_crash_counts(df=df, grouping_cols=['Country']).query('Crashes >= 5')
+    if us_exclude_flag:
+        crash_counts = crash_counts.query('Country != "United States of America"')
+    fig = px.treemap(
+        crash_counts, 
+        path=[px.Constant('World'), 'Country'], 
+        values='Crashes',
+        title='Crashes by country',
+        color_continuous_scale=heatmap_colour,
+        color='Crashes',
+        branchvalues='total',
+        height=600, width=700
+    )
+    st.plotly_chart(fig, use_container_width=True)
+    
+def crashes_by_cont_country_treemap(us_exclude_flag):
+    crash_counts = find_crash_counts(df=df, grouping_cols=['Continent','Country']).query('Crashes >= 5')
+    if us_exclude_flag:
+        crash_counts = crash_counts.query('Country != "United States of America"')
+    fig = px.treemap(
+        crash_counts, 
+        path=[px.Constant('World'), 'Continent', 'Country'], 
+        values='Crashes',
+        title='Crashes by continents and country',
+        color_continuous_scale=heatmap_colour,
+        color='Crashes',
+        branchvalues='total',
+        height=600, width=700
+    )
+    st.plotly_chart(fig, use_container_width=True)
+    
+def crashes_by_state_map():
+    with open('US_states_abbrv.json', 'r') as file:
+        state_abbr = json.load(file)
+    df_filtered = df.query('Country == "United States of America"')
+    df_US_crash = find_crash_counts(df=df, grouping_cols='US_State')
+    df_US_crash['StateAbbr'] = df_US_crash['US_State'].map(state_abbr)
+
+    fig = px.choropleth(
+        df_US_crash,
+        locations='StateAbbr',
+        locationmode='USA-states',
+        color='Crashes',
+        hover_name='US_State',
+        color_continuous_scale=heatmap_colour,
+        scope='usa',
+        title='Plane crashes in the US'
+    )
+    st.plotly_chart(fig, use_container_width=True)
     
 if __name__ == "__main__":
     crashes_per_year()
     crashes_per_day_number()
     
+    # The graphs for month and day of week are rather small, so we put them in a single row, in different columns.
     col1, col2 = st.columns(2)
     
     with col1:
@@ -209,12 +275,25 @@ if __name__ == "__main__":
     crashes_per_decade()
     
     # For the year-month heatmap, we split the diagram into unequal parts.
-    separation = 4
+    separation = 5
     st.markdown('## Crashes per year and month')
     decade_values = df['Decade'].unique().tolist()[::-separation]
     for decade in decade_values:
         crashes_per_year_and_month(decade, separation)
+    # crashes_per_year_and_month(decade_values[-1], separation, 500)
     
     crashes_per_month_weekday()
     crashes_per_month_day()
+    
+    st.markdown('# Crashes by country')
+    st.write('Since the US takes a large majority of the crashes, you can choose to turn off its influence.')
+    us_exclude_flag = st.checkbox(label='Exclude the US in maps?', value=True) 
+    crashes_by_country_map(us_exclude_flag)
+    crashes_by_country_treemap(us_exclude_flag)
+    
+    st.markdown('# Crashes by continent and country')
+    crashes_by_cont_country_treemap(us_exclude_flag)
+    
+    st.markdown('# Crashes in the US')
+    crashes_by_state_map()
     
