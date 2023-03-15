fe04bfddddb0f8574e21655acab70e38f60a7dc2
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-03-15 20:25:35 +0545

    Pages update
    Drastically reduced the LOC for each page

--- pages/Survival_rates.py
+++ pages/Survival_rates.py
@@ -1,32 +1,4 @@
-import streamlit as st
+from page_template import Template
 
-# Read the list of colors from a text file
-with open("Colours_list_real.txt", "r") as f:
-    colors = [line.strip() for line in f]
-
-# Define the size of the grid
-grid_size = 10
-
-# Define the size of each color box
-box_size = 30
-
-# Define the style for the color box
-box_style = f"width:{box_size}px; height:{box_size}px; margin:0px;"
-
-# Define the style for the color name
-name_style = f"text-align:center; font-size:12px; margin-top:5px; margin-bottom:0px;"
-
-# Create a table for the grid
-table = "<table>"
-for i in range(grid_size):
-    # Add a row for the color boxes and names
-    table += "<tr>"
-    for j in range(grid_size):
-        color = colors[i*grid_size + j]
-        table += f"<td style='{name_style}'>{color}</td>"
-        table += f"<td style='{box_style} background-color:{color};'></td>"
-    table += "</tr>"
-table += "</table>"
-
-# Display the table
-st.write(table, unsafe_allow_html=True)
+page = Template(measure='Survival_rate', agg_func='mean')
+page.make_page(main_title='Survival rates', target_type=None)
