c361f4d3bf7df8beda9afdb1b32a4af37d13f1ad
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-03-16 18:20:25 +0545

    Added files for template
    Made a separate module for constructing all the three visualisation pages

---
+++ page_template.py
@@ -0,0 +1,246 @@
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+from utils import aggregate_columns, find_crash_counts
+import random
+import json
+import numpy as np
+from plot_creator import PlotMaker
+
+
+# LOAD THE DATASET
+df = pd.read_parquet('Processed_dataset/Crash_data_new.parquet')
+
+class Template:
+    
+    def __init__(self, measure, agg_func):
+        st.set_page_config(layout="wide")
+        self.df = df
+        with open('Colours_list_real.txt', 'r') as file:
+            values = file.readlines()
+            self.CSS_colours = [value.strip() for value in values]
+            
+        # For the continous colour scales (for heatmaps)
+        self.continuous_colours = px.colors.named_colorscales()
+        self.continuous_colours.sort()
+
+        # Find the index of the default scale - "Jet"
+        self.heatmap_colour_default = self.continuous_colours.index('jet')
+        
+        self._make_sidebar()
+        self.plotter = PlotMaker(df=self.df, measure=measure, agg_func=agg_func, continuous_colour=self.heatmap_colour, discrete_colour=self.plot_colour)
+    
+    def _make_sidebar(self):
+        with st.sidebar:
+            self.colour_choice = st.selectbox(
+                label='Use CSS colours or hex colours?',
+                options=['CSS', 'Hex'],
+                index=0
+            )
+            if self.colour_choice == 'Hex':
+                self.plot_colour = st.color_picker(label='Colour to be used in the plots')
+            else:
+                self.plot_colour = st.selectbox(
+                    label='Select the plot colour',
+                    options=self.CSS_colours
+                )
+            self.heatmap_colour = st.selectbox(
+                label='Select the colour scale for heatmaps',
+                options=self.continuous_colours,
+                index=self.heatmap_colour_default
+            )
+            self.figure_height = st.slider(
+                label='Select the figure heights',
+                min_value=400, max_value=800,
+                value=500,
+                step=50
+            )
+            
+    def _make_filters(self):
+        st.markdown('Apply the filters you want.')
+        col1, col2, col3, col4, col5, col6, col7= st.columns(7)
+        with col1:
+            self.country_filter = st.checkbox(label='Country/Region', value=False)
+        with col2:
+            self.continent_filter = st.checkbox(label='Continent', value=False)
+        with col3:
+            self.year_filter = st.checkbox(label='Year', value=False)
+        with col4:
+            self.month_filter = st.checkbox(label='Month', value=False)
+        with col5:
+            self.day_filter = st.checkbox(label='Day of week', value=False)
+        with col6:
+            self.day_num_filter = st.checkbox(label='Day', value=False)
+        with col7:
+            self.decade_filter = st.checkbox(label='Decade', value=False)
+
+        self.passenger_filter = st.checkbox(label='Commercial flights', value=False)
+            
+        self.location_filters = [self.country_filter, self.continent_filter]
+
+        # Create the options for the selectboxes
+        # For locations
+        self.df['Continent'] = self.df['Continent'].fillna(value='Unknown')
+        country_list = sorted(self.df['Country'].unique())
+        continent_list = sorted(self.df['Continent'].unique())
+        self.selected_country, self.selected_continent = False, False
+        
+        # For dates
+        year_list = sorted(self.df['Date'].dt.year.unique())
+        month_list = self.df['Month'].sort_values().unique().tolist()
+        day_list = self.df['Day_of_week'].sort_values().unique().tolist()
+        day_num_list = sorted(self.df['Date'].dt.day.unique())
+        decade_list = sorted(self.df['Decade'].unique())
+        self.selected_year, self.selected_month, self.selected_day, self.selected_day_num, self.selected_decade = [False for _ in range(5)]
+
+        # Make filters for locations
+        if self.country_filter:
+            self.selected_country = st.selectbox(label='Select the country (or region)', options=country_list)
+            self.plotter.df = self.plotter.df.query("Country in @self.selected_country")
+        if self.continent_filter:
+            self.selected_continent = st.selectbox(label='Select the continent', options=continent_list)
+            self.plotter.df = self.plotter.df.query('Continent in @self.selected_continent')
+            
+        # Make filters for date and times
+        if self.year_filter:
+            self.selected_year = st.selectbox(label='Select the year', options=year_list)
+            self.plotter.df = self.plotter.df.query("Date.dt.year == @self.selected_year")
+        if self.month_filter:
+            self.selected_month = st.selectbox(label='Select the month', options=month_list)
+            self.plotter.df = self.plotter.df.query("Month == @self.selected_month")
+        if self.day_filter:
+            self.selected_day = st.selectbox(label='Select the day', options=day_list)
+            self.plotter.df = self.plotter.df.query("Day_of_week == @self.selected_day")
+        if self.day_num_filter:
+            self.selected_day_num = st.selectbox(label='Select the day number', options=day_num_list)
+            self.plotter.df = self.plotter.df.query("Date.dt.day == @self.selected_day_num")
+        if self.decade_filter:
+            self.selected_decade = st.selectbox(label='Select the decade', options=decade_list)
+            self.plotter.df = self.plotter.df.query("Decade == @self.selected_decade")
+            
+        # For non-military flights
+        if self.passenger_filter:
+            self.plotter.df = self.plotter.df.query('Type == "Passenger"')
+            
+    def _make_year_line_plot(self):
+        st.markdown('## By year')
+        self.plotter.draw_line_plot(grouping_col=self.plotter.df['Date'].dt.year, title='Per year', date_name='Year', height=self.figure_height)
+            
+    def _update_colours(self):
+        self.plotter.continuous_colour = self.heatmap_colour
+        self.plotter.discrete_colour = self.plot_colour
+        
+    def _make_main_title(self, title):
+        st.markdown(f'# {title}')
+        
+    def _make_date_tabs(self):
+        st.markdown('## By date or time')
+        decade_tab, month_tab, day_tab, day_num_tab, time_tab = st.tabs([
+            'Decade', 'Month', 'Day of week', 'Day number', 'Time of day'
+        ])
+        
+        # If a particular date filter is applied, omit making the corresponding graph since it has only one column.
+        error_message = '## Not applicable because of the applied filter(s).'
+        with month_tab:
+            if self.month_filter:
+                st.write(error_message)
+            else:
+                self.plotter.draw_histogram(grouping_col='Month', title='Per month', nbins=12, date_name=None, height=self.figure_height)
+        with day_tab:
+            if self.day_filter:
+                st.write(error_message)
+            else:
+                self.plotter.draw_histogram(grouping_col='Day_of_week', title='Per day', nbins=7, date_name=None, height=self.figure_height)
+        with day_num_tab:
+            if self.day_num_filter:
+                st.write(error_message)
+            else:
+                self.plotter.draw_histogram(grouping_col=self.df['Date'].dt.day, nbins=31, title='Per day number', date_name='Day', height=self.figure_height)
+        
+        # Give an explanation about the x-axis of the time histogram.
+        time_tab_message = """
+            A pandas column cannot be represented purely in just time - a date needs to be attached to this time data. \n
+            As a result, the date in the x-axis is assumed to be the 1st of January, 1900. \n
+            In other words, all the crashes are assumed to have occured on this date at different times of day.
+        """
+        with time_tab:
+            with st.expander('A notice regarding the x-axis'):
+                st.markdown(time_tab_message)
+            self.plotter.draw_time_histogram(nbins=24*2, title='Time of day', height=self.figure_height)
+            
+        with decade_tab:
+            if self.decade_filter or self.year_filter:
+                st.write(error_message)
+            else:
+                self.plotter.draw_histogram(grouping_col='Decade', nbins=15, title='Per decade', height=self.figure_height)
+                            
+    def _make_geo_maps(self):
+        # Create a worldmap only if all an individual country is not selected
+        if self.selected_country:
+            return
+        st.markdown('## By countries')
+        us_exclude_flag = st.checkbox('Exclude the US from world map?')
+        # Make an expander to show the reason.
+        with st.expander('Explanation:'):
+            message = """
+                There are a lot of data points of the US compared to other countries.
+                Including the US will heavily skew the colour palette in the favour of the US.
+                Excluding it will somewhat remove the skew.
+            """
+            st.write(message)
+        self.plotter.draw_world_map(us_exclude_flag=us_exclude_flag, grouping_col='Country', title='Crashes throughout the world')
+
+        # Show the map of US only if all countries are selected or North America has been selected.
+        if (self.selected_country == 'United States of America' or
+            self.selected_continent == 'North America' or
+            not any(self.location_filters)):
+            st.markdown('## By US states')
+            self.plotter.draw_US_map(title='Crashes throughout the US')
+            
+    def _make_heatmaps(self, type_conversion: str = None):
+        """
+        Arguments:
+            type_conversion: The target type of the measure column. Must be one of `int32`, `float64` or `None`
+        """
+        date_filters = [self.year_filter, self.month_filter, self.day_filter, self.day_num_filter]
+        condition = all(date_filters) or not any(date_filters)
+        if not condition:
+            return
+        st.markdown('## Heatmaps')
+        self.show_values = st.checkbox('Show values?', value=False)
+        year_month, month_day, month_day_number = st.tabs([
+            'Year and month', 'Month and day', 'Month and day number'
+        ])
+        with year_month:
+            self.plotter.draw_heatmap_year_month(title='Year and month', height=self.figure_height, target_type=type_conversion, show_value=self.show_values)
+        with month_day:
+            self.plotter.make_heatmap_month_day(title='Month and day', height=self.figure_height, target_type=type_conversion, show_value=self.show_values)
+        with month_day_number:
+            self.plotter.make_heatmap_month_day_number(title='Month and day number', height=self.figure_height, target_type=type_conversion, show_value=self.show_values)
+    
+    def _make_treemaps(self):
+        if self.country_filter:
+            return
+        st.markdown('## Treemaps')
+        us_exclude = st.checkbox('Ignore the US?', value=False)
+        self.plotter.draw_country_treemap(height=self.figure_height, us_exclude_flag=us_exclude)
+        self.plotter.draw_continent_country_treemap(height=self.figure_height, us_exclude_flag=us_exclude)
+        
+
+    def make_page(
+        self, 
+        main_title: str, 
+        target_type: str, 
+        treemap_flag: bool = True
+    ):
+        self._make_main_title(main_title)
+        self._make_filters()
+        if not self.year_filter:
+            self._make_year_line_plot()
+        self._make_date_tabs()
+        self._make_geo_maps()
+        self._make_heatmaps(target_type)
+        if treemap_flag:
+            self._make_treemaps()
+        
--- plot_creator.py
+++ plot_creator.py
@@ -7,6 +7,7 @@ import pandas as pd
 import plotly.express as px
 import json
 import plotly.graph_objects as go
+import numpy as np
 
 
 class PlotMaker:
@@ -34,8 +35,20 @@ class PlotMaker:
         self.agg_func = agg_func
         self.continuous_colour = continuous_colour
         self.discrete_colour = discrete_colour
+        self.type_map = {
+            'int32': np.int32,
+            'float64': np.float64
+        }
         
     def aggregate_dataframe(self, grouping_cols: list[str], date_name: str=None, us_flag=None):
+        """
+        Aggregates the dataframe by the provided grouping columns and measure.
+        
+        Arguments:
+            grouping_cols: The columns to group by.
+            date_name: Renames the `Date` column to `date_name`
+            us_flag: Whether to include the US in maps.
+        """
         if us_flag is None:
             df_to_use = self.df
         else:
@@ -56,9 +69,14 @@ class PlotMaker:
             )
         return df_agg
     
-    def draw_histogram(self, grouping_col: str, title: str, nbins: int, date_name: str=None, size: list[int]=[None, None]):
+    def draw_histogram(self, grouping_col: str, title: str, nbins: int, date_name: str = None, height: int = None):
         df_agg = self.aggregate_dataframe(grouping_cols=grouping_col, date_name=date_name)
         x = grouping_col if date_name is None else date_name
+        hist_funcs_map = {
+            'sum': 'sum',
+            'mean': 'avg'
+        }
+        hist_func = hist_funcs_map.get(self.agg_func)
         fig = px.histogram(    
             data_frame=df_agg,
             x=x,
@@ -66,12 +84,13 @@ class PlotMaker:
             text_auto=True,
             nbins=nbins,
             title=title,
+            histfunc=hist_func,
             color_discrete_sequence=[self.discrete_colour],
-            width=size[0], height=size[1]
+            height=height
         ).update_traces(marker=dict(line=dict(color='black', width=1))).update_layout(bargap=0.2)
         st.plotly_chart(fig, use_container_width=True)
         
-    def draw_line_plot(self, grouping_col: str, title: str, date_name: str = None, size: list[int] = [None, None]):
+    def draw_line_plot(self, grouping_col: str, title: str, date_name: str = None, height: int = None):
         df_agg = self.aggregate_dataframe(grouping_cols=grouping_col, date_name=date_name)
         
         fig = px.line(
@@ -81,7 +100,7 @@ class PlotMaker:
             markers=True,
             title=title,
             color_discrete_sequence=[self.discrete_colour],
-            width=size[0], height=size[1],
+            height=height,
         )
         st.plotly_chart(fig, use_container_width=True)
         
@@ -124,7 +143,7 @@ class PlotMaker:
             df_US_agg,
             locations='StateAbbr',
             locationmode='USA-states',
-            color='Crashes',
+            color=self.measure,
             hover_name='US_State',
             color_continuous_scale=self.continuous_colour,
             scope='usa',
@@ -135,15 +154,14 @@ class PlotMaker:
         )
         st.plotly_chart(fig, use_container_width=True)
         
-    def _return_heatmap(self, matrix, title=None, size: list[int]=[None, 500]) -> go.Figure:
+    def _return_heatmap(self, matrix, title=None, height: int = None, show_value: bool = False) -> go.Figure:
         fig = px.imshow(
-                    matrix, 
-                    aspect='equal',
-                    text_auto=True, 
-                    color_continuous_scale=self.continuous_colour,
-                    width=size[0],
-                    height=size[1],
-                    title=title
+            matrix, 
+            aspect='equal',
+            text_auto=show_value, 
+            color_continuous_scale=self.continuous_colour,
+            height=height,
+            title=title
         )
         return fig
         
@@ -151,7 +169,9 @@ class PlotMaker:
         self, 
         divisions: int=4,
         title: str=None, 
-        size: list[int]=[None, None],
+        height: int = None,
+        target_type: str = None,
+        show_value: bool = False
     ):
         """
         Make a heatmap of year vs month.
@@ -160,43 +180,56 @@ class PlotMaker:
             division: The number of parts to divide the total number of decades with.
             title: The title
             size: A list consisting of [width, height]
+            target_type: The type that the measure column should be converted to. Must be one of `int32`, `float64` or `None`
         """
         decades = self.df['Decade'].unique().tolist()
         decade_values = decades[::-divisions]
+
         for decade in decade_values:
             decade_to_draw = [decade + 10*i for i in range(0, divisions)]
             df_agg = self.aggregate_dataframe(grouping_cols=[self.df.query('Decade in @decade_to_draw')['Date'].dt.year, 'Month'], date_name='Year')
+            measure_dtype = self.type_map.get(target_type)
+            df_agg[self.measure] = df_agg[self.measure].astype(measure_dtype)
             matrix = df_agg.pivot_table(
                 index='Month',
                 columns='Year',
                 values=self.measure
             )
-            figure = self._return_heatmap(matrix=matrix, title=title, size=size)
+            figure = self._return_heatmap(matrix=matrix, title=title, height=height, show_value=show_value)
             st.plotly_chart(figure, use_container_width=True)
             
-    def make_heatmap_month_day(self, title=None, size: list[int]=[None, 500]):
+    def make_heatmap_month_day(self, title=None, height: int = None, target_type: str = None, show_value: bool = False):
         df_agg = self.aggregate_dataframe(grouping_cols=['Month', 'Day_of_week'])
+        measure_dtype = self.type_map.get(target_type)
+        df_agg[self.measure] = df_agg[self.measure].astype(measure_dtype)
         matrix = df_agg.pivot_table(
             index='Day_of_week',
             columns='Month',
             values=self.measure
         )
-        figure = self._return_heatmap(matrix=matrix, title=title, size=size)
+        figure = self._return_heatmap(matrix=matrix, title=title, height=height, show_value=show_value)
         st.plotly_chart(figure, use_container_width=True)
         
-    def make_heatmap_month_day_number(self, title=None, size: list[int]=[None, 500]):
+    def make_heatmap_month_day_number(self, title=None, height: int = None, target_type: str = None, show_value: bool = False):
         df_agg = self.aggregate_dataframe(grouping_cols=['Month', self.df['Date'].dt.day], date_name='Day')
+        measure_dtype = self.type_map.get(target_type)
+        df_agg[self.measure] = df_agg[self.measure].astype(measure_dtype)
         matrix = df_agg.pivot_table(
             columns='Day',
             index='Month',
             values=self.measure
         )
-        figure = self._return_heatmap(matrix=matrix, title=title, size=size)
+        figure = self._return_heatmap(matrix=matrix, title=title, height=height, show_value=show_value)
         st.plotly_chart(figure, use_container_width=True)
         
-    def draw_time_histogram(self, nbins: int, title: str=None, size: list[int]=[None, None]):
+    def draw_time_histogram(self, nbins: int, title: str=None, height: int = None):
         df_time = self.aggregate_dataframe(grouping_cols='Time')
         df_time['Time'] = pd.to_datetime(df_time['Time'], format="%H:%M:%S").sort_values()
+        hist_funcs_map = {
+            'sum': 'sum',
+            'mean': 'avg'
+        }
+        hist_func = hist_funcs_map.get(self.agg_func)
         fig = px.histogram(    
             data_frame=df_time,
             x='Time',
@@ -205,14 +238,41 @@ class PlotMaker:
             nbins=nbins,
             title=title,
             color_discrete_sequence=[self.discrete_colour],
-            width=size[0], height=size[1]
+            histfunc=hist_func,
+            height=height
         ).update_traces(marker=dict(line=dict(color='black', width=1))).update_layout(bargap=0.2)
         st.plotly_chart(fig, use_container_width=True)
         
-         
-    
-# df = pd.read_parquet('Processed_dataset/Crash_data_new.parquet')
-# obj = PlotMaker(df=df, measure='Crashes', agg_func=None, continuous_colour=None, discrete_colour=None)
-
-# obj = PlotMaker(df=df, measure='Total_fatalities', agg_func='sum', continuous_colour='#4e4e4e', discrete_colour=None)
-# obj.draw_line_plot(grouping_col=df['Date'].dt.year, title='Test', date_name='Year')
+    def draw_country_treemap(self, us_exclude_flag: bool = False, height: int = None):
+        df_agg = self.aggregate_dataframe(grouping_cols='Country')
+        if us_exclude_flag:
+            df_agg = df_agg.query('Country != "United States of America"')
+        self.country_treemap_threshold = st.number_input(label=f'Enter the threshold value for `{self.measure}`', key=1, step=1)
+        df_agg = df_agg.query(f'{self.measure} >= @self.country_treemap_threshold')
+        fig = px.treemap(
+            df_agg, 
+            path=[px.Constant("world"), 'Country'], 
+            values=self.measure,
+            color=self.measure,
+            title='By country',
+            color_continuous_scale=self.continuous_colour,
+            height=height
+        )
+        st.plotly_chart(fig, use_container_width=True)
+        
+    def draw_continent_country_treemap(self, us_exclude_flag: bool = False, height: int = None):
+        df_agg = self.aggregate_dataframe(grouping_cols=['Continent', 'Country'])
+        if us_exclude_flag:
+            df_agg = df_agg.query('Country != "United States of America"')
+        df_agg[self.measure] = df_agg[self.measure].astype('int32')
+        df_agg = df_agg.query(f'{self.measure} >= @self.country_treemap_threshold')
+        fig = px.treemap(
+            df_agg, 
+            path=[px.Constant("world"), 'Continent', 'Country'], 
+            values=self.measure,
+            color=self.measure,
+            title='By continent and country',
+            color_continuous_scale=self.continuous_colour,
+            height=height
+        )
+        st.plotly_chart(fig, use_container_width=True)
