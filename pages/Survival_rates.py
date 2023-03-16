faaa3ab3c482caafd4a00a09e40d102e2e38fee6
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-03-16 18:18:36 +0545

    Small revision to survival rates page

--- pages/Survival_rates.py
+++ pages/Survival_rates.py
@@ -1,4 +1,4 @@
 from page_template import Template
 
 page = Template(measure='Survival_rate', agg_func='mean')
-page.make_page(main_title='Survival rates', target_type=None)
+page.make_page(main_title='Survival rates', target_type=None, treemap_flag=False)
